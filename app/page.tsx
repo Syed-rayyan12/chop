433f2c9cd1fb7b402fc344480b9ed8734b06aa8a
Syed-rayyan12 <148645321+Syed-rayyan12@users.noreply.github.com>
2025-09-12 23:27:38 +0500

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 
 import { RestaurantRiderNavbar } from "@/components/customer-panel-components/admin-rider-navbar";
+import ComingSoon from "@/components/customer-panel-components/coming-soon";
 import { FeaturedRestaurants } from "@/components/customer-panel-components/featured-restaurants";
 import { Footer } from "@/components/customer-panel-components/footer";
 import { Header } from "@/components/customer-panel-components/header";
 
 import { HeroSection } from "@/components/customer-panel-components/hero-section";
+import MobileSection from "@/components/customer-panel-components/mobile-section";
 import { PopularCuisines } from "@/components/customer-panel-components/popular-cuisines";
 
 export default function Page() {
@@ -16,6 +18,8 @@ export default function Page() {
         <HeroSection />
         <FeaturedRestaurants />
         <PopularCuisines />
+        <MobileSection/>
+        <ComingSoon/>
       </main>
       <Footer />
     </div>
