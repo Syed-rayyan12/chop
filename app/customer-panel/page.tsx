b71dbe522bca200f640951f6f52597679186f4fa
Syed-rayyan12 <148645321+Syed-rayyan12@users.noreply.github.com>
2025-09-23 16:07:26 +0500

    updated

--- app/customer-panel/page.tsx
+++ app/customer-panel/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import React from 'react'
-import { RestaurantRiderNavbar } from "@/components/customer-panel-components/admin-rider-navbar";
 import { FeaturedRestaurants } from "@/components/customer-panel-components/featured-restaurants";
 import { Footer } from "@/components/customer-panel-components/footer";
 import { Header } from "@/components/customer-panel-components/header";
 import { HeroSection } from "@/components/customer-panel-components/hero-section";
 import { PopularCuisines } from "@/components/customer-panel-components/popular-cuisines";
+import MobileSection from '@/components/customer-panel-components/mobile-section';
+import Marquee from '@/components/customer-panel-components/marquee';
+import ComingSoon from '@/components/customer-panel-components/coming-soon';
 
 const page = () => {
     return (
@@ -14,9 +16,15 @@ const page = () => {
                 {/* <RestaurantRiderNavbar /> */}
                 <Header />
                 <main>
+                    <>
                     <HeroSection />
                     <FeaturedRestaurants />
                     <PopularCuisines />
+                    <MobileSection/>
+                    <Marquee/>
+                    <ComingSoon/>
+                    
+                    </>
                 </main>
                 <Footer />
             </div>
--- components/customer-panel-components/footer.tsx
+++ components/customer-panel-components/footer.tsx
@@ -5,7 +5,7 @@ import { Facebook, Twitter, Instagram, Smartphone, Monitor } from "lucide-react"
 export function Footer() {
   return (
     <footer className="bg-white border-t border-primary/50">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <div className=" mx-auto px-4 sm:px-6 lg:px-36 py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Company Info */}
           <div>
